a85366f083dd9d1b55409f4611a53ae0a7cf198a
Marcus Berro <marcusberro@gmail.com>
2015-12-21 22:48:21 -0200

    Fixing bin/bash reference

--- restart-db.sh
+++ restart-db.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 kill $(ps aux | grep mongod | grep config | grep mongodb-single | awk '{print $2}')
 sleep 1
 mongod --config mongodb-single.conf --fork
--- shutdown-db.sh
+++ shutdown-db.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 kill $(ps aux | grep mongod | grep config | grep mongodb-single | awk '{print $2}')
